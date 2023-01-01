7bf611a576c046e772645d7c6588517a09094aca
withoutspaces <dms@ic.ufal.br>
2023-01-01 18:17:47 -0300

    feat: add router to app component

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,11 @@ import { db } from './config/firebase'
 import { collection, getDocs } from 'firebase/firestore'
 import React from 'react'
 import { signInWithGoogle } from './auth/login-provider'
-import Home from './pages/Home'
+import Router from './routes/Router'
 
 function App() {
-
-  React.useEffect(() => {
-    const getItems = async () => {
-      const col = collection(db, 'items');
-      const snapshot = await getDocs(col);
-      const list = snapshot.docs.map(doc => doc.data());
-      console.log(list)
-      // console.log(list);
-    }
-    getItems();
-  }, [])
-
-  
   return (
-    <Home/>
+    <Router/>
   )
 }
 
