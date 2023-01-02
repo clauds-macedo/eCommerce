5b8c2cb0e434ebe7cd698a6d20f572ddab5eb22c
withoutspaces <dms@ic.ufal.br>
2023-01-02 14:36:28 -0300

    bug: id doesn't exist

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,11 +8,16 @@ interface ModalProps {
   deleteButton: () => void
 }
 
+interface IEventTarget extends React.MouseEvent<Element, MouseEvent> {
+  id: string
+}
 
 function Modal({id = 'modal', onClose, deleteButton }: ModalProps) {
 
-  const handleOutsideClick = (e: React.ChangeEvent<HTMLDivElement>) => {
-    if(e.target.id === id) onClose()
+  const handleOutsideClick = (e: IEventTarget) => {
+    if(e.target.id === id)
+      console.log(e.target);
+    
   }
 
   return(
@@ -21,7 +26,7 @@ function Modal({id = 'modal', onClose, deleteButton }: ModalProps) {
       onClick={(e) => handleOutsideClick(e)}
       className="w-screen h-screen bg-opacity-90 bg-neutral-900 absolute z-10 flex justify-center items-center "
     >
-      <div className="flex items-center flex-col justify-around w-96 h-48 bg-slate-50 rounded-3xl relative">
+      <div className="flex items-center flex-col justify-around w-96 h-48 bg-slate-50 rounded-3xl z-20 relative">
         <button 
           onClick={() => onClose()}
           className="absolute right-4 top-4"
