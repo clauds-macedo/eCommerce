d48cf0f7d1b20659a263947d2966869fc24c01f3
clauds-macedo <yanalogic@gmail.com>
2023-01-02 10:28:55 -0300

    feat: spinner on fallback

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,28 +1,40 @@
-import {lazy, Suspense} from "react";
+import { Spinner } from "@chakra-ui/react";
+import { lazy, Suspense } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-const Home = lazy(() => import("../pages/Home")) 
-const ProductsForm = lazy(() => import("../pages/ProductsForm")) 
+const Home = lazy(() => import("../pages/Home"));
+const ProductsForm = lazy(() => import("../pages/ProductsForm"));
 
 const suspenseScreens: { [key: string]: JSX.Element } = {
-  home: <Home/>,
-  productsForm: <ProductsForm/>
-}
+  home: <Home />,
+  productsForm: <ProductsForm />,
+};
 
-const suspenseComponent = (child: "home" | "productsForm") =>
-  <Suspense fallback={<h1>Carregando...</h1>}>
+const suspenseComponent = (child: "home" | "productsForm") => (
+  <Suspense
+    fallback={
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    }
+  >
     {suspenseScreens[child]}
   </Suspense>
+);
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={suspenseComponent("home")}/>
-          <Route path="/admin" element={suspenseComponent("productsForm")}/>
+        <Route path="/" element={suspenseComponent("home")} />
+        <Route path="/admin" element={suspenseComponent("productsForm")} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default Router
+export default Router;
