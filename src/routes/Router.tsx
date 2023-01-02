368502fe944c932c38aa6854f0e48d6060bea926
clauds-macedo <yanalogic@gmail.com>
2023-01-02 10:20:52 -0300

    fix: lazy loading working with suspense components

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -4,14 +4,22 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 const Home = lazy(() => import("../pages/Home")) 
 const ProductsForm = lazy(() => import("../pages/ProductsForm")) 
 
+const suspenseScreens: { [key: string]: JSX.Element } = {
+  home: <Home/>,
+  productsForm: <ProductsForm/>
+}
+
+const suspenseComponent = (child: "home" | "productsForm") =>
+  <Suspense fallback={<h1>Carregando...</h1>}>
+    {suspenseScreens[child]}
+  </Suspense>
+
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Suspense fallback={<h1>Carregando...</h1>}> */}
-          <Route path="/" element={<Home />}/>
-          <Route path="/admin" element={<ProductsForm />}/>
-        {/* </Suspense> */}
+          <Route path="/" element={suspenseComponent("home")}/>
+          <Route path="/admin" element={suspenseComponent("productsForm")}/>
       </Routes>
     </BrowserRouter>
   )
